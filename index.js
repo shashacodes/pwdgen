4d72b00252a04c0a12a672d83402d7839bc98043
Ibanga Sharon <ibsharon19@gmail.com>
2023-05-14 16:31:20 +0100

    feat: add new changes

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-let strings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()'
+   let strings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()';
+   let noSpecial = strings.slice(0,62);
+      const passwordPara = document.getElementById('password');
+      const includeRadio = document.getElementById('include');
+      const excludeRadio = document.getElementById('exclude');
+      const button = document.getElementById('btn')
 
-function genpwd(){
-    return "password"
-}
+      function generatePassword() {
+        const includeSpecialChars = includeRadio;
+        const excludeSpecialChars = excludeRadio;
+        const passwordLength = 8;
+        let password = '';
+
+        for (let x = 0; x < passwordLength; x++) {
+          let Numbers = Math.floor(Math.random() * strings.length);
+          let characters = strings[Numbers];
+
+         if (includeSpecialChars.checked) {
+          strings;
+         }
+       else if (excludeSpecialChars.checked) {
+        Numbers = Math.floor(Math.random() * noSpecial.length);
+        characters = noSpecial[Numbers];
+    }
+         password += characters;
+        }
+
+        passwordPara.textContent = password;
+      }
+      btn.addEventListener('click', generatePassword)
